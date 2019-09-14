280a6d1b583a2ae3843f8ab7698df97e6e61c7a5
Lucas Pimenta <pimentalucas@hotmail.com>
2019-09-14 17:07:55 -0300

    Correção

--- Iccanobif.c
+++ Iccanobif.c
@@ -2,30 +2,23 @@
 
 #include <stdio.h>
 
-#define TAMANHO 40
-int iccanobif[TAMANHO];
-
-void icc()
-{
-    iccanobif[0] = 1;
-    iccanobif[1] = 1;
-    for(int i = 2; i<= TAMANHO; i++)
-        iccanobif[i] = iccanobif[i-1] + iccanobif[i-2];
-}
-
-
 int main()
 {
-    int N;
-    icc();
+    int N, i;
+   
     // 1<= N <= 40
     scanf(" %d", &N);
 
-    for(int i = N-1; i>=0; i--)
-    {
-        printf("%d",iccanobif[i]);
-        if(i!=0)
-            printf(" ");
-    }
+    int iccanobif[40] = { 0 };
+
+    iccanobif[0] = 1;
+    iccanobif[1] = 1;
+    for(i = 2; i< N; i++)
+        iccanobif[i] = iccanobif[i-1] + iccanobif[i-2];
+
+    for(i = N-1; i>=1; --i)
+        printf("%d ",iccanobif[i]);
+    
+    printf("%d\n",iccanobif[i]);
 
 }
